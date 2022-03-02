2277a73cd9798abfe4cbb9291b404c0ee2bd8f9a
bangalia <anitabangali@yahoo.com>
2022-03-01 18:43:49 -0600

    Initial Commit

---
+++ Brain Picker/Brain_PickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brain_PickerApp.swift
+//  Brain Picker
+//
+//  Created by Anita Bangali on 3/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Brain_PickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Brain Picker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Brain Picker
+//
+//  Created by Anita Bangali on 3/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
