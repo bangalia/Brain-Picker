e5e52f8e21927a448ac01b32af3baca541677845
bangalia <anitabangali@yahoo.com>
2022-03-05 12:31:48 -0600

    added delegation files

---
+++ Brain Picker/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  Brain Picker
+//
+//  Created by Anita Bangali on 3/5/22.
+//
+
+import Foundation
--- Brain Picker/GameController.swift
+++ Brain Picker/GameController.swift
@@ -9,6 +9,14 @@ import Foundation
 import UIKit
 
 class ViewController: UIViewController {
-        
+    let colorMeaning = ["red", "orange", "yellow", "blue", "purple", "green"]
+    let colorDict: [String:UIColor] = [
+        "red": UIColor.red,
+        "orange": UIColor.orange,
+        "green": UIColor.green,
+        "yellow": UIColor.yellow,
+        "blue": UIColor.blue,
+        "purple": UIColor.purple
+    ]
     }
 
